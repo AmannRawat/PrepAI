ec04567c0d35d741dd9d918d377d6b870b19da89
AmannRawat <amanrawat@gmail.com>
2025-08-19 20:04:41 +0530

    Tailwind Debugging
    Using New approach of applying tailwind css and custom css configurations by going through official tailwind documentations

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,24 +1,26 @@
-    /** @type {import('tailwindcss').Config} */
-    export default {
-      content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-      ],
-      theme: {
-        extend: {
-          colors: {
-            'background': '#24283b',
-            'surface': '#4F4242',
-            'accent': '#E64833',
-            'text-primary': '#FBE9D0',
-            'text-secondary': '#90AEAD',
-          },
-          fontFamily: {
-            sans: ['Inter', 'sans-serif'],
-            mono: ['"Fira Code"', 'monospace'],
-          }, //HERE
-        },
-      },
-      plugins: [],
-    }
-    
+//NOTE: TAILWIND CONFIGURATION FILE IS NOT REQUIRED FOR NEW VERSION OF TAILWIND CSS All CONGIF IS DONE IN INDEX.css
+// Webiste https://tailwindcss.com/docs/colors
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         'background' : '#24283b',
+//         'surface': '#4F4242',
+//         'accent': '#E64833',
+//         'text-primary': '#FBE9D0',
+//         'text-secondary': '#90AEAD',
+//       },
+//       fontFamily: {
+//         sans: ['Inter', 'sans-serif'],
+//         mono: ['"Fira Code"', 'monospace'],
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
