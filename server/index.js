b9e2e0b3ec6f2d1afe32f8e66b8e0d55b9c2dd5a
AmannRawat <amanrawat1509@gmail.com>
2025-11-19 20:22:19 +0530

    Pre Deployement process

--- server/index.js
+++ server/index.js
@@ -22,12 +22,18 @@ const flashModel = genAI.getGenerativeModel({ model: "gemini-2.0-flash-exp" });
 const proModel = genAI.getGenerativeModel({ model: "gemini-2.5-pro" });
 const storage = multer.memoryStorage(); // Store files in memory
 const upload = multer({ storage: storage });
-const app = express();
 const PORT = process.env.PORT || 8000;
 
 
+const app = express();
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    // In production, this set FRONTEND_URL in Vercel.
+    // Locally, it defaults to localhost.
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
 
 //MONGO DB CONNECTION
